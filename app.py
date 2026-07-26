e31124bb9c2a7e04cd82df752994927908eda14f
Aidan Zhou <aidan2006@tamu.edu>
2026-07-25 19:26:17 -0500

    Basic posts working

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import streamlit as st
 import api
 
-st.set_page_config(page_title="Getaway Carbs", layout="wide")
+st.set_page_config(page_title="Getaway Carbs")
 st.title("Getaway Carbs")
 
-st.write(api.get_posts())
+posts = api.get_posts()
+
+for post in posts:
+    box = st.container(border=True)
+    with box:
+        st.title(post["author"]["username"])
+        st.write(post["restaurant"])
+        st.write(post["order"])
+        st.write(f'Leaving at {post["departure"]}')
+        st.write(post["notes"])
+
+
