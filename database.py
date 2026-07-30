60d51eb068a2b7d1e4a8f2613e268f610658f66f
Aidan Zhou <aidan2006@tamu.edu>
2026-07-29 19:51:58 -0500

    Trying Database again

--- database.py
+++ database.py
@@ -9,6 +9,13 @@ DATABASE_URL = os.getenv(
     "sqlite:///./carbs.db",
 )
 
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1,
+    )
+
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(
         DATABASE_URL,
